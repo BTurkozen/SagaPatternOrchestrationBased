d048e776d55cf17b9de5e87f364505b980c43426
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-27 20:39:58 +0300

    - OrderStateMachine oluşturuldu. - Program.cs içerisinde RabitMq, Entityframe ve Saga implementasyonlarını gerçekleştirdik.

--- SagaStateMachineWorkerService/Program.cs
+++ SagaStateMachineWorkerService/Program.cs
@@ -1,8 +1,39 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using SagaStateMachineWorkerService;
+using SagaStateMachineWorkerService.Models;
+using Shared.Settings;
+using System.Reflection;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((hostContext, services) =>
     {
+        services.AddMassTransit(cfg =>
+        {
+            cfg.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>().EntityFrameworkRepository(opt =>
+            {
+                opt.AddDbContext<DbContext, OrderStateDbContext>((provider, builder) =>
+                {
+                    builder.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnectionStr"), m =>
+                    {
+                        m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+                    });
+                });
+            });
+
+            cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configure =>
+            {
+                configure.Host(hostContext.Configuration.GetConnectionString("RabbitMq"));
+
+                configure.ReceiveEndpoint(RabbitMqSettingsConst.OrderSaga, e =>
+                {
+                    e.ConfigureSaga<OrderStateInstance>(provider);
+                });
+            }));
+        });
+
+
+
         services.AddHostedService<Worker>();
     })
     .Build();
