1cad5d871b70e8452d176a417c68f72d2b427eb8
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-27 20:10:13 +0300

    - OrderStateInstance oluşturma ve body'si yazıldı.

---
+++ SagaStateMachineWorkerService/Models/OrderStateInstance.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachineWorkerService.Models
+{
+    public class OrderStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; }
+        public int BuyerId { get; set; }
+        /// <summary>
+        /// Odeme tamamlandıktan sonra order service'e bir event gönderilecek ve Status'unu completed olarak ayarlıyacak.
+        /// </summary>
+        public int OrderId { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public override string ToString()
+        {
+            var properties = GetType().GetProperties();
+
+            var sb = new StringBuilder();
+
+            properties.ToList().ForEach(p =>
+            {
+                var value = p.GetValue(this, null);
+
+                sb.Append($"{p.Name}:{value}");
+            });
+
+            sb.Append("-----------------------");
+
+            return sb.ToString();
+        }
+    }
+}
