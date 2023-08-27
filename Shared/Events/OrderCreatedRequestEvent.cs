d4498b5e73fb14d6a21c115ed000a848a3ad00ab
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-27 19:37:22 +0300

    - state machine içerisine gerekli kütüphaneler eklendi. - masstransit ve entityframework - OOrderCreatedrequestEvent oluşturuldu ve imlement edildi.

---
+++ Shared/Events/OrderCreatedRequestEvent.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Interfaces;
+using Shared.Messages;
+
+namespace Shared.Events
+{
+    public class OrderCreatedRequestEvent : IOrderCreatedRequestEvent
+    {
+        public OrderCreatedRequestEvent()
+        {
+            OrderItems = new List<OrderItemMessage>();
+        }
+
+        public int OrderId { get; set; }
+        public string BuyerId { get; set; }
+        public List<OrderItemMessage> OrderItems { get; set; }
+        public PaymentMessage Payment { get; set; }
+    }
+}
---
+++ Shared/Settings/RabbitMqSettingsConst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Settings
+{
+    public class RabbitMqSettingsConst
+    {
+        public const string OrderSaga = "order-saga-queue";
+    }
+}
