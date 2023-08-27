3ad7e9b38dd11330086503d2906e74582c0a0381
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-27 20:21:17 +0300

    - SagaStateInstance için datacontext nesnesi ve SagaStateMapping oluşturuldu.

---
+++ SagaStateMachineWorkerService/Models/OrderStateMap.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachineWorkerService.Models
+{
+    public class OrderStateMap : SagaClassMap<OrderStateInstance>
+    {
+        protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+        {
+            entity.Property(p => p.BuyerId).HasMaxLength(256);
+        }
+    }
+}
